e4fd08dd0340e1c387277b447d069b21c235ff7f
Shashwat Anand <120978465+Sushiii07@users.noreply.github.com>
2023-07-07 16:01:28 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+df = pd.read_csv('50_Startups.csv')
+
+# Check for NULL values
+print(pd.isnull(df))
+
+# OR
+
+# In the below method we can see number of entries matches number of non-null values
+# Therefore, number of NULL values is 0
+print(df.info())
+
+# Removing rows containing any NULL values
+df2 = df.dropna()
+
+# Check for duplicated values
+print(df2.duplicated())
+# Number of duplicated values
+print("Number of duplicate rows =", df2.duplicated().sum())
+
+# Removing duplicate rows (if any)
+df.drop_duplicates(inplace=True)
+
+# Final Data
+print(df2)
